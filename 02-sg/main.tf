189cedd3f358327d2e37ae5289c4760ccbb56f88
Pradeep-kanta <kantapradeep07@gmail.com>
2024-04-02 19:51:32 +0530

    infra-dev

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -147,6 +147,15 @@ resource "aws_security_group_rule" "app_alb_vpn" {
   security_group_id        = module.app_alb.sg_id
 }
 
+resource "aws_security_group_rule" "app_alb_web" {
+  source_security_group_id = module.web.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
 #openvpn
 resource "aws_security_group_rule" "vpn_home" {
   security_group_id = module.vpn.sg_id
